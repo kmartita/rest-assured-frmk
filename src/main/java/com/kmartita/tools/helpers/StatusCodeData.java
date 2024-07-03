6b542016e2ab164c417262998f4ece3bf8186251
Marta Kravchuk <martakravchuk@Martas-MacBook-Pro.local>
2024-07-04 00:02:11 +0300

    +1 case

--- src/main/java/com/kmartita/tools/helpers/StatusCodeData.java
+++ src/main/java/com/kmartita/tools/helpers/StatusCodeData.java
@@ -3,7 +3,9 @@ package com.kmartita.tools.helpers;
 public interface StatusCodeData {
     String OK = "HTTP/1.1 200 OK";
     String BAD_REQUEST = "HTTP/1.1 400 Bad Request";
+    String INTERNET_SERVER_ERROR = "HTTP/1.1 500 Internal Server Error";
 
     int STATUS_CODE_200 = 200;
     int STATUS_CODE_400 = 400;
+    int STATUS_CODE_500 = 500;
 }
--- src/test/java/com/kmartita/spaces/BaseSpaceTest.java
+++ src/test/java/com/kmartita/spaces/BaseSpaceTest.java
@@ -2,9 +2,15 @@ package com.kmartita.spaces;
 
 import com.kmartita.BaseTest;
 import com.kmartita.tools.data.Entity;
+import com.kmartita.tools.data.bodyschemas.spaces.SpaceFields;
+import com.kmartita.tools.data.generation.models.TestData;
 import io.qameta.allure.Step;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.util.Set;
+
+import static com.kmartita.tools.Utils.getRequiredFields;
 
 public class BaseSpaceTest extends BaseTest {
 
@@ -21,5 +27,14 @@ public class BaseSpaceTest extends BaseTest {
     @Step("Delete all spaces")
     private void deleteSpaces() {
         deleteEntities(entityService.getSpaces(team), Entity.SPACE);
+    }
+
+    public TestData<SpaceFields> generateSpaceDataWithRequiredFields(){
+        Set<SpaceFields> requiredFields = getRequiredFields(SpaceFields.class);
+        return TestData.preGenerate(requiredFields).build();
+    }
+
+    public TestData<SpaceFields> generateSpaceDataExcludingFeatures(){
+        return generateSpaceDataWithRequiredFields().removeFields(SpaceFields.FEATURES);
     }
 }
